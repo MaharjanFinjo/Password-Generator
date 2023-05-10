0a07d3d929d12ca168c5b758dc6348e95f942a51
Maharjan <mahamoto.jp@gmail.com>
2023-05-10 21:44:01 +0900

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+import { characters } from "./character.js";
 
+console.log(characters);
 
+let pass1 = document.getElementById("password1");
 
+let pass2 = document.getElementById("password2");
 
+let PassBtn = document.getElementById("btn");
+
+let secondClick = false;
+
+PassBtn.addEventListener("click", function () {
+  Render();
+  secondClick = true;
+});
+
+function Render() {
+  if (secondClick) {
+    pass1.textContent = "";
+    pass2.textContent = "";
+    for (let i = 0; i < 15; i++) {
+      let random1 = Math.floor(Math.random() * characters.length);
+      let random2 = Math.floor(Math.random() * characters.length);
+
+      pass1.textContent += characters[random1];
+      pass2.textContent += characters[random2];
+      console.log(i);
+      secondClick = true;
+    }
+  } else {
+    for (let i = 0; i < 15; i++) {
+      let random1 = Math.floor(Math.random() * characters.length);
+      let random2 = Math.floor(Math.random() * characters.length);
+
+      pass1.textContent += characters[random1];
+      pass2.textContent += characters[random2];
+      console.log(i);
+      secondClick = true;
+    }
+  }
+}
